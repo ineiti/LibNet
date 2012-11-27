233c3579d8ec7902b68f1748e5b62b667fcbeab4
Linus Gasser <ineiti@linusetviviane.ch>
2012-11-27 23:02:48 +0100

    Taking into account ISP-settings

--- LibNet.rb
+++ LibNet.rb
@@ -1,25 +1,57 @@
+#!/usr/bin/ruby -wKU
 # This is the LibNet-wrapper for the bash-scripts that wrap the
 # gateway-functionality of the internet-router.
 # The bash-scripts are thought to be used in a wireless-router, too.
 
 require 'drb'
 
-class LibNet
-  def initialize
-    %x[ ./lib_net captive_setup ]
+if not respond_to? :dputs
+  DEBUG_LVL=3
+  
+  def ddputs(a,&b)
+    puts yield b
   end
-	
-	def call( func, reply = nil )
-		if reply
-		  return %x[ ./lib_net print #{reply} func #{func}]
-		else
-		  return %x[ ./lib_net func #{func}]
-		end
-	end
+  
+  def dputs(a,&b)
+    if a <= DEBUG_LVL
+      puts yield b
+    end
+  end
 end
 
-DRb.start_service 'druby://:9000', LibNet.new
-puts "Server running at #{DRb.uri}"
+class LibNet
+  def initialize
+    @dir = File.dirname( __FILE__ )
+    %x[ #{@dir}/lib_net func captive_setup ]
+  end
+	
+  def call( func, reply = nil )
+    dputs(3){ "Called with #{func} - #{reply}" }
+    s = ""
+    if reply
+      s = "print #{reply}"
+    end
+    if func
+      s += " func #{func}"
+    end
+    dputs(4){ "Going to call #{s}" }
+    return %x[ #{@dir}/lib_net #{s} ].chomp
+  end
+
+  def call_args( func, argstr )
+    dputs(4){ "Going to call_args #{func} - #{argstr}" }
+    return %x[ #{@dir}/lib_net func #{func} #{argstr} ].chomp
+  end
+	
+  def status
+    "OK"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME 
+  DRb.start_service 'druby://:9000', LibNet.new
+  dputs(0){ "Server running at #{DRb.uri}" }
  
-trap("INT") { DRb.stop_service }
-DRb.thread.join
+  trap("INT") { DRb.stop_service }
+  DRb.thread.join
+end
