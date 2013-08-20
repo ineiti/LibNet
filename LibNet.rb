a15b50124fa6f41a6d2c3224f5d96c66938b958b
Linus Gasser <ineiti@linusetviviane.ch>
2013-08-20 12:30:00 +0100

    Added printing of course-related sheets

--- LibNet.rb
+++ LibNet.rb
@@ -10,7 +10,7 @@ SIMUL=true
 if not respond_to? :dputs
   DEBUG_LVL=3
   
-  def ddputs(a,&b)
+  def dputs(a,&b)
     puts yield b
   end
   
@@ -28,18 +28,17 @@ class LibNet
     # hope it's done in 1 second
     if not SIMUL
       Process.detach( Process.fork { 
-        ddputs(3){"killing"}
-        %x[ #{@dir}/lib_net kill ]
-        ddputs(3){"init"}
-        %x[ #{@dir}/lib_net ping ]
-        ddputs(3){"finished"}
-      } )
-      exit
+          dputs(3){"killing"}
+          %x[ #{@dir}/lib_net kill ]
+          dputs(3){"init"}
+          %x[ #{@dir}/lib_net ping ]
+          dputs(3){"finished"}
+        } )
       sleep 1
-      ddputs(3){"setting up"}
+      dputs(3){"setting up"}
       %x[ #{@dir}/lib_net func captive_setup ]
       @env = call_print( :ENV )
-      ddputs(3){"Env is at #{@env}"}
+      dputs(3){"Env is at #{@env}"}
     else
       dputs(1){"Simulation only"}
     end
@@ -52,7 +51,7 @@ class LibNet
   end
 
   def async( func, *args )
-    ddputs(3){ "Async with #{func} - #{args.inspect}" }
+    dputs(3){ "Async with #{func} - #{args.inspect}" }
     SIMUL and return ""
     return %x[ #{@dir}/lib_net async #{func} #{args.join( ' ' )} ].chomp
   end
@@ -75,7 +74,7 @@ class LibNet
   end
 
   #def call_args( func, argstr )
-  #  ddputs(4){ "Going to call_args #{func} - #{argstr}" }
+  #  dputs(4){ "Going to call_args #{func} - #{argstr}" }
   #  return %x[ #{@dir}/lib_net func #{func} #{argstr} ].chomp
   #end
 	
@@ -90,13 +89,13 @@ class LibNet
   def isp_params
     SIMUL and return {}
     Hash[ %w( ISP CONNECTION_TYPE HAS_PROMO HAS_CREDIT ALLOW_FREE ).collect{|v|
-      [ v, print( v ) ]
-    } ]
+        [ v.downcase, print( v ) ]
+      } ]
   end
 
   def get_var_file( var )
     SIMUL and return []
-    if file = print( var )
+    if ( file = print( var ) ) and File.exists?( file )
       return IO.readlines( file )
     end
     return []
