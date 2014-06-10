cc77b773fc5cb5acaa2604bbba80b0204520f84e
Linus Gasser <ineiti@linusetviviane.ch>
2014-06-10 21:46:01 +0100

    merging from hg

--- Tools/9dnsmasq-internet.sh
+++ Tools/9dnsmasq-internet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/perl -pe "s/address=\/#/#address=\/#/" /opt/profeda/LibNet/Tools/dnsmasq.conf > /tmp/dnsmasq.conf
+/usr/bin/perl -pe "s/address=\/#/#address=\/#/" /etc/dnsmasq.conf.orig > /tmp/dnsmasq.conf
 mv /tmp/dnsmasq.conf /etc/dnsmasq.conf
 if [ -x /usr/bin/systemctl ]; then
   systemctl restart dnsmasq
